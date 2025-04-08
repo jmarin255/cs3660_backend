b2c58ad75c99a536e17ac75d2e11d92ce30bb5c5
jmarin255 <143770508+jmarin255@users.noreply.github.com>
2025-04-07 20:16:12 -0600

    connected the backend with frontend (#1)
    Co-authored-by: Juan Marin <juanmarin@Juans-MacBook-Pro.local>

--- main.py
+++ main.py
@@ -1,46 +1,40 @@
-from fastapi import FastAPI, HTTPException
-import json
-from pydantic import BaseModel
+from fastapi import FastAPI
+from fastapi import FastAPI
+from fastapi.openapi.utils import get_openapi
+from config import settings
+
 from fastapi.middleware.cors import CORSMiddleware
 
-app=FastAPI()
+from controllers import login_controller
+from middleware import api_gateway_middleware
+from middleware.auth_middleware import AuthMiddleware
+from schemas.message_schema import MessageResponse
+
+
+app = FastAPI(title="CS3660 Backend Project", version="1.0.0")
+
+
+app.add_middleware(AuthMiddleware)
+
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173"],  
     allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"]
-
+    allow_methods=["*"],  
+    allow_headers=["*"],  
 )
 
-@app.get("/")
 
+
+app.include_router(login_controller.router)
+
+
+@app.get("/", response_model=MessageResponse)
 def read_root():
-    return {"message": "hello world"}
+    return {"message": "Hello, FastAPI!"}
 
+@app.get("/health", response_model=MessageResponse)
+def health():
+    return {"message": "Ok"}
 
-class loginRequest(BaseModel):
-    username: str
-    password: str
-
-
-def verify_login(username: str, password: str) -> bool:
-    try:
-        with open("./db/users.json") as file:
-            data=json.load(file)
-            for user in data ["users"]:
-                if user["username"]== username and user ["password"]==password:
-                    return True
-    except FileNotFoundError:
-        raise HTTPException(status_code=500, detail="User file not found")
-
-    return False
-
-
-
-@app.post("/api/login")
-def login(request: loginRequest): 
-    if (verify_login(request.username, request.password)):
-        return {'success': 'true'}
-    raise HTTPException(status_code=401, detail="invalid credentials")
--- repositories/user_repository.py
+++ repositories/user_repository.py
@@ -1,15 +1,20 @@
 import json
+
 from models.user_model import User
+
+
 
 class UserRepository:
     @staticmethod
     def get_user_by_username(username: str) -> User:
         try:
-            with open ("./db/users.json", "r") as file:
-                data= json.load(file)
-                for user in data ["users"]:
-                    if user["username"]== username:
-                        return User(user["username"],user["name"],user["password_hash"])
+            with open("./db/users.json", "r") as file:
+                data = json.load(file)
+                for user in data["users"]:
+                    if user["username"] == username:
+                        return User(user["username"], user["name"], user["password_hash"])  # Returns the User object
         except FileNotFoundError:
-            raise Exception ("User file not found")
+            raise Exception("User file not found")
         return None
+    
+    
