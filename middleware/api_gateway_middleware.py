20e25013b8e9b99d6824e5a8b9b814ead48b1cfb
Juan Marin <juanmarin@Juans-MacBook-Pro.local>
2025-04-14 20:48:05 -0600

    added api gateway

--- middleware/api_gateway_middleware.py
+++ middleware/api_gateway_middleware.py
@@ -4,13 +4,16 @@ from starlette.middleware.base import BaseHTTPMiddleware
 
 from config import settings
 
-app=FastAPI()
+app = FastAPI()
 
 class ApiGatewayAuthMiddleware(BaseHTTPMiddleware):
     def __init__(self, app: FastAPI):
         super().__init__(app)
 
-    async def dispatch(self, request: Request, call_next):
-        api_token_header= request.headers.get("x-api-token")
+    async def dispatch(self, request: Request, call_next):      
+        api_token_header = request.headers.get("x-api-token")
         if not api_token_header or api_token_header != settings.api_gateway_token:
-            return JSONResponse(status_code=403, content={"Detail": "invalid api token" })
+            
+            return JSONResponse(status_code=403, content={"detail": "invalid api token"})         
+
+        return await call_next(request)
