2d26e9eef7984366c528c75f185bfb1b4474c706
Juan Marin <juanmarin@Juans-MacBook-Pro.local>
2025-04-17 02:34:28 -0600

    fixed code

--- models/user_model.py
+++ models/user_model.py
@@ -1,4 +1,15 @@
-class User:
+from sqlalchemy import Column, Integer, String
+
+from models.base_model import Base
+
+
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    username = Column(String(50), unique=True, nullable=False)
+    name = Column(String(100), nullable=False)
+    password_hash = Column(String(512), nullable=False)
+
     def __init__(self, username: str, name: str, password_hash: str):
         self.username = username
         self.name = name        
