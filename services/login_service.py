069a74a0aaf1169fe0d3997df6cb6cf96de96244
jmarin255 <143770508+jmarin255@users.noreply.github.com>
2025-04-17 23:13:08 -0600

    Updated login_service.py

--- services/login_service.py
+++ services/login_service.py
@@ -2,25 +2,29 @@ import hashlib
 import jwt
 import datetime
 from repositories.user_repository import UserRepository
+
+
 from config import settings
 
 class LoginService:
     def __init__(self, user_repository: UserRepository):
         self.user_repository = user_repository
 
+
+    # Function to verify a jwt token
     @staticmethod
     def verify_token(token: str) -> dict:
-        """Verify a JWT token and decode its payload."""
         try:
+            # Decode token
             payload = jwt.decode(token, settings.secret_key, algorithms=[settings.algorithm])
             return payload
         except jwt.ExpiredSignatureError:
             raise Exception("Token has expired")
         except jwt.InvalidTokenError:
             raise Exception("Invalid token")
-
+        
+    # Function to verify login from users.json    
     def get_login_token(self, username: str, password: str) -> str:
-        """Generate a JWT token after verifying user credentials."""
         try:
             # Fetch user from database
             user = self.user_repository.get_user_by_username(username)
@@ -33,23 +37,21 @@ class LoginService:
             # Verify password match
             if user.password_hash != hashed_password:
                 raise Exception("Invalid credentials")
-
+            
             user_payload = {
                 "username": user.username,
                 "name": user.name
             }
 
-            # Generate JWT token with expiration time
+            # Generate JWT token
             expiration_time = datetime.datetime.now(datetime.timezone.utc) + datetime.timedelta(hours=1)
             token_payload = {
                 "sub": user.username,  # Subject (user)
                 "exp": expiration_time,  # Expiry time
-                "user": user_payload  # Include user attributes if needed
+                "user": user_payload  # Include role or other user attributes if needed
             }
             token = jwt.encode(token_payload, settings.secret_key, algorithm=settings.algorithm)
 
             return token
         except Exception as e:
             raise Exception(f"Login failed: {str(e)}")
-
-
